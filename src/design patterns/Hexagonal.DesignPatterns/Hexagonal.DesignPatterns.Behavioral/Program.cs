54389970ff46c26378175f2c6340c3928ce82ee9
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-06-19 03:13:16 +0530

    Behavioral project added to solution

---
+++ src/design patterns/Hexagonal.DesignPatterns/Hexagonal.DesignPatterns.Behavioral/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hexagonal.DesignPatterns.Behavioral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
