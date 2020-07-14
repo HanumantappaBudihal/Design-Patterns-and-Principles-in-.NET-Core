3eac83776acaee383fd9b06d94b2a8b720a95b53
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-07-15 00:32:01 +0530

    Added the EF Core to application ( Need to add the related to EF Core and SQL server referencet to project nuget package)

---
+++ src/design patterns/Hexagonal.DesignPatterns/RepositoryDesingSample/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RepositoryDesingSample.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int? Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ src/design patterns/Hexagonal.DesignPatterns/RepositoryDesingSample/Models/LearningContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RepositoryDesingSample.Models
+{
+    public partial class LearningContext : DbContext
+    {
+        public LearningContext()
+        {
+        }
+
+        public LearningContext(DbContextOptions<LearningContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=MSI\\LEARNING;Database=Learning;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.Department)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
