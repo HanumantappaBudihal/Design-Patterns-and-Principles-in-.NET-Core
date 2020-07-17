88bdd6202305f7679aa36dfbac657a9c50413a09
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-07-17 23:54:23 +0530

    Setup code base for Depedency injectino implementation using the Unity IoC Container in .NET mvc application

---
+++ src/design patterns/Hexagonal.DesignPatterns/Hexagonal.DesignPatterns.DIUsingUnityIoC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Hexagonal.DesignPatterns.DIUsingUnityIoC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/design patterns/Hexagonal.DesignPatterns/Hexagonal.DesignPatterns.DIUsingUnityIoC/App_Start/UnityConfig.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Web.Mvc;
+using Unity;
+using Unity.AspNet.Mvc;
+
+namespace Hexagonal.DesignPatterns.DIUsingUnityIoC
+{
+    /// <summary>
+    /// Specifies the Unity configuration for the main container.
+    /// </summary>
+    public static class UnityConfig
+    {
+        #region Unity Container
+        private static Lazy<IUnityContainer> container =
+          new Lazy<IUnityContainer>(() =>
+          {
+              var container = new UnityContainer();
+              RegisterTypes(container);
+              return container;
+          });
+
+        /// <summary>
+        /// Configured Unity Container.
+        /// </summary>
+        public static IUnityContainer Container => container.Value;
+        #endregion
+
+        /// <summary>
+        /// Registers the type mappings with the Unity container.
+        /// </summary>
+        /// <param name="container">The unity container to configure.</param>
+        /// <remarks>
+        /// There is no need to register concrete types such as controllers or
+        /// API controllers (unless you want to change the defaults), as Unity
+        /// allows resolving a concrete type even if it was not previously
+        /// registered.
+        /// </remarks>
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            // NOTE: To load from web.config uncomment the line below.
+            // Make sure to add a Unity.Configuration to the using statements.
+            // container.LoadConfiguration();
+
+            // TODO: Register your type's mappings here.
+            // container.RegisterType<IProductRepository, ProductRepository>();
+        }
+
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
