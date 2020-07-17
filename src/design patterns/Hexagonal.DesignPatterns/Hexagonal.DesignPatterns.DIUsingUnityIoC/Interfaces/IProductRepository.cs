880f2390ac7dc10eb55e38d23926f012f99a0b9f
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-07-18 01:16:20 +0530

    Added all related interfaces to DI Project

---
+++ src/design patterns/Hexagonal.DesignPatterns/Hexagonal.DesignPatterns.DIUsingUnityIoC/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Hexagonal.DesignPatterns.DIUsingUnityIoC.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<IProduct> GetAll();
+        IProduct Get(int id);
+        IProduct Add(IProduct item);
+        bool Update(IProduct item);
+        bool Delete(int id);
+    }
+}
