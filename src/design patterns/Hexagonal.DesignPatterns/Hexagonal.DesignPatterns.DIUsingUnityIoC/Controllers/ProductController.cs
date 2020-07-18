b3feac5d6688501dcde8027b60a8d3b6f8bc88fd
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-07-18 20:15:44 +0530

    DI Implementation using the Unity framework

---
+++ src/design patterns/Hexagonal.DesignPatterns/Hexagonal.DesignPatterns.DIUsingUnityIoC/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Hexagonal.DesignPatterns.DIUsingUnityIoC.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hexagonal.DesignPatterns.DIUsingUnityIoC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        //inject dependency - Constructor inject
+        public ProductController(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = _productRepository.GetAll();
+            return View(products);
+        }
+    }
+}
---
+++ src/design patterns/Hexagonal.DesignPatterns/Hexagonal.DesignPatterns.DIUsingUnityIoC/Models/ProductRepository.cs
@@ -0,0 +1,69 @@
+﻿using Hexagonal.DesignPatterns.DIUsingUnityIoC.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Hexagonal.DesignPatterns.DIUsingUnityIoC.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<IProduct> products = new List<IProduct>();
+        private int _nextId = 1;
+
+        public ProductRepository()
+        {
+            // Add products for the Demonstration
+            Add(new Product { Name = "Computer", Category = "Electronics", Price = 23.54M });
+            Add(new Product { Name = "Laptop", Category = "Electronics", Price = 33.75M });
+            Add(new Product { Name = "iPhone4", Category = "Phone", Price = 16.99M });
+        }
+
+        public IEnumerable<IProduct> GetAll()
+        {
+            return products;
+        }
+
+        public IProduct Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public IProduct Add(IProduct product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            product.Id = _nextId++;
+            products.Add(product);
+
+            return product;
+        }
+
+        public bool Update(IProduct product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            int index = products.FindIndex(p => p.Id == product.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            products.RemoveAt(index);
+            products.Add(product);
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            //Need to hadle properly
+            //Add the only required to code to undestand the simple
+            products.RemoveAll(p => p.Id == id);
+            return true;
+        }
+    }
+}
